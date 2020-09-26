c6098a135ec2194ac4e0c581e229b910e36a91af
Daniel Frey <dfrey@pivotal.io>
2020-09-26 16:48:57 -0400

    set context loads test to 'test' profile, no dependency on mysql this way

--- src/test/java/com/pksheldon4/sample/leaderselection/LeaderElectDemoApplicationTests.java
+++ src/test/java/com/pksheldon4/sample/leaderselection/LeaderElectDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.pksheldon4.sample.leaderselection;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles( "test" )
 class LeaderElectDemoApplicationTests {
 
     @Test
